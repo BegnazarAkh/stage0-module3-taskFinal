35e827d74ff35f218fa3d6f5676f4bf5da60f3b4
BegnazarAkh <begnazaraxmadjonov@mail.ru>
2023-08-22 15:15:22 +0200

    Stage 0 Module 3 Final Task

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -3,10 +3,10 @@ package lang.print.gaps.finalModuleTask;
 public class NumbersPrinter {
     public void printIsPositive(int number) {
         if(number > 0){
-            System.out.println("True");
+            System.out.println("true");
         }
         else{
-            System.out.println("False");
+            System.out.println("false");
         }
     }
 }
